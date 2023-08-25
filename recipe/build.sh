85ad5ab361b7797468637a8d24559986610d8fe4
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-25 13:14:25 +0200

    Migrate to pnpm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-if [[ "$target_platform" == osx* ]];
-then
-    mkdir ~/.npm-global
-    npm config set prefix '~/.npm-global'
-    export PATH=~/.npm-global/bin:$PATH
+set -euxo pipefail
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export npm_config_arch="arm64"
 fi
 
-cd frontend
-tgz=$(npm pack)
-npm install -g $tgz
+# Don't use pre-built gyp packages
+export npm_config_build_from_source=true
+
+rm $PREFIX/bin/node
+ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+
+NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
+
+pnpm import
+pnpm install
+pnpm pack
+npm install -g ${PKG_NAME}-${PKG_VERSION}.tgz
+pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
